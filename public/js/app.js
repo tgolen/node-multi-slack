f9f4d54a45c0a63d4ca9a14e82469d10d3024492
Tim Golen <tgolen@expensify.com>
2017-01-02 15:24:15 -0700

    Add a view for everyone's events

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,25 @@
+var allUsers = [];
+
 /**
  * Gets all of the dates for a user and puts them into our data object
  * @param  {Object} user
  * @param  {Object} data
  */
 function mixinDataForUser(user, data) {
-    if (USER.events && USER.events.length) {
-        for (var i = 0; i < USER.events.length; i++) {
-            var start = new moment(USER.events[i].start);
-            var end = new moment(USER.events[i].end);
+    if (user.events && user.events.length) {
+        for (var i = 0; i < user.events.length; i++) {
+            var start = new moment(user.events[i].start);
+            var end = new moment(user.events[i].end);
 
-            // Start dates get a high value so they are dark
+            // Add all of our start days
             if (!data[start.unix()]) {
-                data[start.unix()] = 100;
+                data[start.unix()] = 1;
             } else {
-                data[start.unix()] += 100;
+                data[start.unix()] += 1;
             }
 
-            // Subsequent days for that range get a small value
-            if (USER.events[i].start !== USER.events[i].end) {
+            // Add all the subsequent days
+            if (user.events[i].start !== user.events[i].end) {
                 var diffInDays = end.diff(start, 'days');
                 for (var j = 0; j < diffInDays; j++) {
                     var recurringDay = moment(start).add(j + 1, 'day');
@@ -31,7 +33,38 @@ function mixinDataForUser(user, data) {
             }
         }
     }
-};
+}
+
+/**
+ * Displays a list of users who are gone on a specific day
+ * @param  {String} date
+ */
+function displaySelectedDate(date) {
+    var usersGone = {};
+
+    // Loop through every user
+    for (var i = 0; i < allUsers.length; i++) {
+        var user = allUsers[i];
+        if (user.events && user.events.length) {
+            // Loop through every event
+            for (var j = 0; j < user.events.length; j++) {
+                var event = user.events[j];
+                if (moment(date).isBetween(moment(event.start), moment(event.end), 'day', '[]')) {
+                    usersGone[user.id] = user;
+                }
+            }
+        }
+    }
+
+    var $results = $('#cal-day-results');
+    var $resultCollection = $('<ul class="collection">');
+
+    $.each(usersGone, function(userId, user) {
+        $resultCollection.append('<li class="collection-item">'+user.id+'</li>');
+    });
+
+    $results.html($resultCollection);
+}
 
 $(function() {
     var cal = new CalHeatMap();
@@ -39,16 +72,20 @@ $(function() {
 
     // Get the data for our users
     if (window.USER) {
-        mixinDataForUser(window.USER, data);
+        allUsers.push(window.USER);
     } else if (window.USERS) {
-        for (var i = 0; i < window.USERS.length; i++) {
-            mixinDataForUser(window.USERS[i], data);
-        }
+        allUsers = window.USERS;
+    }
+
+    for (var i = 0; i < allUsers.length; i++) {
+        mixinDataForUser(allUsers[i], data);
+        mixinDataForUser(allUsers[i], data);
     }
 
     // Initialize our calendar
     cal.init({
         domain: 'month',
         data: data,
+        onClick: displaySelectedDate,
     });
 });
--- routes/calendar.js
+++ routes/calendar.js
@@ -1,12 +1,12 @@
 var db = require('../lib/db').db();
 
 module.exports = function route(req, res) {
-    db.collection('users').find({}, function(err, users) {
+    db.collection('users').find().toArray(function(err, users) {
         if (err) {
             return console.error(err);
         }
 
-        res.locals.USER = user;
+        res.locals.USERS = users;
 
         res.render('calendar', {
             userId: req.params.userId
