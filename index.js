860252cafc560122bf47e7cb449f23f958babc04
Tim Golen <tgolen@expensify.com>
2017-01-02 13:10:37 -0700

    Add event and calendar pages

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var mainController = require('./controllers/main');
 
 if (!process.env.SLACKBOT_TOKEN) {
@@ -11,12 +12,19 @@ mainController.start();
 var app = express();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+app.use(favicon(__dirname + '/public/images/logo.png'));
 
 app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'Titan'
-    });
-})
+    res.render('index');
+});
+
+app.get('/events', function (req, res) {
+    res.render('events');
+});
+
+app.get('/calendar', function (req, res) {
+    res.render('calendar');
+});
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
