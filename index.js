ac6b74cc6d1df79e01bdc27ec7e86e6029ebf3ac
Tim Golen <tgolen@expensify.com>
2017-01-02 13:05:36 -0700

    Add a static web page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var express = require('express');
 var mainController = require('./controllers/main');
 
 if (!process.env.SLACKBOT_TOKEN) {
@@ -6,3 +7,17 @@ if (!process.env.SLACKBOT_TOKEN) {
 }
 
 mainController.start();
+
+var app = express();
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Titan'
+    });
+})
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening on port 3000!');
+});
