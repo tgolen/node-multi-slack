669111bd8f940064615c5fcbc80d96fd8854db09
Tim Golen <tgolen@expensify.com>
2017-01-06 08:15:05 -0700

    Add some code commenting

--- routes/10am.js
+++ routes/10am.js
@@ -25,82 +25,73 @@ module.exports = function (req, res) {
             return;
         }
 
-        var newUpdate = {
-            user: user.id,
-            text: req.body.text,
-            created_at: new Date(),
-        };
-        db.collection('updates').insertOne(newUpdate, function(err) {
+        // Record the last time the user made an update and what that update was
+        user.lastUpdate_at = new Date();
+        user.lastUpdate = req.body.text;
+        controller.storage.users.save(user, function(err) {
             if (err) {
                 console.error(err);
                 res.send('Ooops, there was an error. How embarassing. ' + err.toString());
                 return;
             }
 
-            user.lastUpdate = new Date();
-            controller.storage.users.save(user, function(err) {
+            // Send their update to our main 10AM channel
+            bot.api.chat.postMessage({
+                channel: '10amtest',
+                text: req.body.text,
+                as_user: false,
+                username: req.body.user_name,
+                icon_url: user.slackUser ? user.slackUser.profile.image_72 : '',
+            }, function(err) {
                 if (err) {
                     console.error(err);
                     res.send('Ooops, there was an error. How embarassing. ' + err.toString());
                     return;
                 }
 
-                bot.api.chat.postMessage({
-                    channel: '10amtest',
-                    text: req.body.text,
-                    as_user: false,
-                    username: req.body.user_name,
-                    icon_url: user.slackUser ? user.slackUser.profile.image_72 : '',
-                }, function(err) {
+                // Respond to the API at this point so the rest is done after the request
+                res.send('OK, I will post your update!');
+
+                // Now send a PM to each of our users with that update
+                controller.storage.users.all(function(err, users) {
                     if (err) {
                         console.error(err);
-                        res.send('Ooops, there was an error. How embarassing. ' + err.toString());
                         return;
                     }
 
-                    res.send('OK, I will post your update!');
+                    if (users && users.length) {
+                        // @TODO Check to make sure the user posting this message hasn't been snoozed by them
+                        // @TODO Check to make sure we aren't sending a message to ourself
+                        for (var i = 0; i < users.length; i++) {
+                            var recipient = users[i];
 
-                    // Now send a PM to each of our users with that update
-                    controller.storage.users.all(function(err, users) {
-                        if (err) {
-                            console.error(err);
-                            return;
+                            // Open an IM channel and post to it
+                            bot.api.im.open({
+                                user: recipient.id
+                            }, function (err, res) {
+                                if (err) {
+                                    console.error(err);
+                                    return;
+                                }
+
+                                var channelId = res.ok ? res.channel.id : null;
+                                if (channelId) {
+                                    bot.api.chat.postMessage({
+                                        channel: channelId,
+                                        text: req.body.text,
+                                        as_user: false,
+                                        username: req.body.user_name,
+                                        icon_url: user.slackUser ? user.slackUser.profile.image_72 : '',
+                                    }, function(err) {
+                                        if (err) {
+                                            console.error(err);
+                                            return;
+                                        }
+                                    });
+                                }
+                            });
                         }
-
-                        if (users && users.length) {
-                            // @TODO Check to make sure the user posting this message hasn't been snoozed by them
-                            // @TODO Check to make sure we aren't sending a message to ourself
-                            for (var i = 0; i < users.length; i++) {
-                                var recipient = users[i];
-
-
-                                bot.api.im.open({
-                                    user: recipient.id
-                                }, function (err, res) {
-                                    if (err) {
-                                        console.error(err);
-                                        return;
-                                    }
-
-                                    var channelId = res.ok ? res.channel.id : null;
-                                    if (channelId) {
-                                        bot.api.chat.postMessage({
-                                            channel: channelId,
-                                            text: req.body.text,
-                                            as_user: false,
-                                            username: req.body.user_name,
-                                            icon_url: user.slackUser ? user.slackUser.profile.image_72 : '',
-                                        }, function(err) {
-                                            if (err) {
-                                                console.error(err);
-                                                return;
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        }
-                    });
+                    }
                 });
             });
         });
