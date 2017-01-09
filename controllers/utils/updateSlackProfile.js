362f18fabded8166fe19beaa3d6e75fb45deff72
Tim Golen <tgolen@expensify.com>
2017-01-09 08:06:56 -0700

    Only update user info every 15 minutes

--- controllers/utils/updateSlackProfile.js
+++ controllers/utils/updateSlackProfile.js
@@ -1,5 +1,6 @@
 var moment = require('moment');
-var cacheOfProcessedUsers = {};
+var userCache = {};
+var cacheTimeInMilliseconds = 1000 * 60 * 15;
 
 /**
  * Every time a user posts a message, we update their slack profile so we can stay up to date on their profile
@@ -8,6 +9,14 @@ var cacheOfProcessedUsers = {};
  * @param  {Object} controller
  */
 module.exports = function updateSlackProfile(bot, message, controller) {
+    // Cache the user updates for 15 minutes so we don't do too much stuff
+    if (userCache[message.user]) {
+        var userCacheExpiration = moment(userCache[message.user]);
+        if (userCacheExpiration.diff(moment(), 'minutes') < 15) {
+            return;
+        }
+    }
+
     controller.storage.users.get(message.user, function(err, user) {
         if (err) {
             return console.trace(err);
@@ -25,7 +34,9 @@ module.exports = function updateSlackProfile(bot, message, controller) {
             }
 
             user.slackUser = res.user;
-            controller.storage.users.save(user, function() {});
+            controller.storage.users.save(user, function() {
+                userCache[user.id] = new Date();
+            });
         });
     });
 };
